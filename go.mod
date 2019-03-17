a621ad1badd6bae8fc384a8d322faa77f6c48763
Louis Tsai <louis993546@gmail.com>
2019-03-17 14:23:12 +0100

    Add generated stuff

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/louistsaitszho/hackernews-graphql
 
 go 1.12
+
+require (
+	github.com/99designs/gqlgen v0.8.1
+	github.com/vektah/gqlparser v1.1.0
+)
---
+++ resolver.go
@@ -0,0 +1,62 @@
+package hackernews_graphql
+
+import (
+	"context"
+) // THIS CODE IS A STARTING POINT ONLY. IT WILL NOT BE UPDATED WITH SCHEMA CHANGES.
+
+type Resolver struct{}
+
+func (r *Resolver) Query() QueryResolver {
+	return &queryResolver{r}
+}
+
+type queryResolver struct{ *Resolver }
+
+func (r *queryResolver) Item(ctx context.Context, id string) (Item, error) {
+	panic("not implemented")
+}
+func (r *queryResolver) Story(ctx context.Context, id string) (*Story, error) {
+	panic("not implemented")
+}
+func (r *queryResolver) Comment(ctx context.Context, id string) (*Comment, error) {
+	panic("not implemented")
+}
+func (r *queryResolver) Ask(ctx context.Context, id string) (*Ask, error) {
+	panic("not implemented")
+}
+func (r *queryResolver) Job(ctx context.Context, id string) (*Job, error) {
+	panic("not implemented")
+}
+func (r *queryResolver) Poll(ctx context.Context, id string) (*Poll, error) {
+	panic("not implemented")
+}
+func (r *queryResolver) Pollopt(ctx context.Context, id string) (*PollOpt, error) {
+	panic("not implemented")
+}
+func (r *queryResolver) User(ctx context.Context, id string) (*User, error) {
+	panic("not implemented")
+}
+func (r *queryResolver) TopStories(ctx context.Context) ([]Story, error) {
+	panic("not implemented")
+}
+func (r *queryResolver) NewStories(ctx context.Context) ([]Story, error) {
+	panic("not implemented")
+}
+func (r *queryResolver) BestStories(ctx context.Context) ([]Story, error) {
+	panic("not implemented")
+}
+func (r *queryResolver) AskStories(ctx context.Context) ([]Ask, error) {
+	panic("not implemented")
+}
+func (r *queryResolver) ShowStories(ctx context.Context) ([]Story, error) {
+	panic("not implemented")
+}
+func (r *queryResolver) JobStories(ctx context.Context) ([]Job, error) {
+	panic("not implemented")
+}
+func (r *queryResolver) UpdatedItems(ctx context.Context) ([]Item, error) {
+	panic("not implemented")
+}
+func (r *queryResolver) UpdatedProfiles(ctx context.Context) ([]User, error) {
+	panic("not implemented")
+}
