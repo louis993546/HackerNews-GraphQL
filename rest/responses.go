862b126b0f84e10b9c104b1e6fa9696d9814afd3
Louis Tsai <louis993546@gmail.com>
2019-06-08 12:00:18 +0200

    Add UserResponse model

--- rest/responses.go
+++ rest/responses.go
@@ -11,3 +11,12 @@ type StoryResponse struct {
 	Type        string `json:"type"`
 	URL         string `json:"url"`
 }
+
+type UserResponse struct {
+	About     string `json:"about"`
+	Created   int    `json:"created"`
+	Delay     int    `json:"delay"`
+	ID        string `json:"id"`
+	Karma     int    `json:"karma"`
+	Submitted []int  `json:"submitted"`
+}
