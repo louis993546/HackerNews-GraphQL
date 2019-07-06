471b83c32e2cc1c9a41ed7762a32995e0c169027
Louis Tsai <louis993546@gmail.com>
2019-07-06 13:17:18 +0200

    Now I know how to get interface working

--- index.js
+++ index.js
@@ -1,25 +1,73 @@
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+var {
+  GraphQLInterfaceType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLObjectType,
+  GraphQLSchema
+} = require('graphql');
 
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello: String
+class Story {
+  constructor(id, title) {
+    this.id = id;
+    this.title = title;
   }
-`);
+}
 
-// The root provides a resolver function for each API endpoint
-var root = {
-  hello: () => {
-    return 'Hello world!';
+class Deleted {
+  constructor(id) {
+    this.id = id
+  }
+}
+
+var MaybeStoryType = new GraphQLInterfaceType({
+  name: 'MaybeStory',
+  fields: { id: { type: GraphQLID } },
+})
+
+var StoryType = new GraphQLObjectType({
+  name: 'Story',
+  interfaces: [MaybeStoryType],
+  fields: {
+    id: { type: GraphQLID },
+    title: { type: GraphQLString },
   },
-};
+  isTypeOf: value => value instanceof Story
+})
+
+var DeletedType = new GraphQLObjectType({
+  name: 'Deleted',
+  interfaces: [MaybeStoryType],
+  fields: { id: { type: GraphQLID } },
+  isTypeOf: value => value instanceof Deleted
+})
+
+var queryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    story: {
+      type: MaybeStoryType,
+      args: {
+        id: { type: GraphQLID }
+      },
+      resolve: (_, { id }) => {
+        var output = new Story(id, "test");
+        console.log("Will return " + JSON.stringify(output));
+        return output
+      }
+    }
+  }
+});
+
+var schema = new GraphQLSchema({ 
+  query: queryType,
+  types: [ MaybeStoryType, StoryType, DeletedType ]
+});
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  rootValue: root,
   graphiql: true,
 }));
 app.listen(4000);
